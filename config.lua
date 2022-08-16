22b38c085c21c5c467f242ae3b0cf96d62bab992
GNRL_Tailya <53150818+Rsawyer2014@users.noreply.github.com>
2022-08-15 20:13:34 -0600

    Update
    Addressed issues, fixed typo's, fixed some queries, added valid checks to some functions

--- config.lua
+++ config.lua
@@ -1,12 +1,12 @@
 Config = {
-    Debug = false, -- Turn debug on / off
+    Debug = true, -- Turn debug on / off
     Multiplier = 5, --The Ammount the total lottery pay out will be, based off total value tickets are sold
     TicketValue = 5000, --- The value the database goes up everytime a ticket is submitted. Best practice to use what the sales price you sell the tickets in stores for
-    Webhook = "", --Webhook for announcing Lotto Pot Value everytime its updated
-    WinHook = "", -- Webhook for where the winner will be posted
+    Webhook = "https://discord.com/api/webhooks/1008891845578862642/s-E7IX9Se27-oEqQlniVLLfJVo0Bb0hOybHcu1oPBPEGU7y19S9pZJNLXq0tJZiWYZeq", --Webhook for announcing Lotto Pot Value everytime its updated
+    WinHook = "https://discord.com/api/webhooks/1008891845578862642/s-E7IX9Se27-oEqQlniVLLfJVo0Bb0hOybHcu1oPBPEGU7y19S9pZJNLXq0tJZiWYZeq", -- Webhook for where the winner will be posted
     DrawDay = 6, -- weekday the draw will be done, 1=sunday, 2 monday, 3 tuesday, 4 wednesday, 5 thursday, 6 friday, 7 saturday
     DrawTime = 19,--The hour it will be drawn at, 24HR Clock
-    AlwaysNotify = false, -- Should the server post new pot every ticket submission? if false, itll only post on server restart
+    AlwaysNotify = true, -- Should the server post new pot every ticket submission? if false, itll only post on server restart
     Command = 'lotterydraw', -- The command to force a lottery draw
 }
 
--- locales/en.lua
+++ locales/en.lua
@@ -3,7 +3,7 @@ local Translations = {
         entered = 'You are Now entered to win!',
         value = "The Dead End Lottery Value today is $ %{payout} With %{submissions} Tickets Submitted",
         command = 'Draw Lottery Winner Manually (Admin Only)',
-        winner = 'The winner of the $ %{payout} is! %{fisrname} %{lastname}'
+        winner = 'The winner of the $ %{payout} is! %{firstname} %{lastname}'
     }
 }
 
