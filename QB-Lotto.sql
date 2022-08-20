ea7b1b39252efdead867516cd4202832d82d6283
GNRL_Tailya <53150818+Rsawyer2014@users.noreply.github.com>
2022-08-20 12:15:28 -0600

    Update QB-Lotto.sql
    Fixed issue with sql file to automate sql

--- QB-Lotto.sql
+++ QB-Lotto.sql
@@ -1,11 +1,11 @@
-CREATE TABLE IF NOT EXISTS `LotteryTickets` (
+CREATE TABLE IF NOT EXISTS `lotterytickets` (
 `id` int(255) NOT NULL AUTO_INCREMENT,
 `citizenid` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `citizenid` (`citizenid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
-CREATE TABLE IF NOT EXISTS `LotteryServer` (
+CREATE TABLE IF NOT EXISTS `lotteryserver` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
   `HasDrawn` varchar(10) DEFAULT NULL,
   `Payout` int(255) DEFAULT NULL,
